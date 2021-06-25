d2dfb511eebc72342d31e2dd156f5226d354ce1c
Leonard Topno CVC <leonardtopno@cvglobal.co>
2021-06-25 14:41:09 +0530

    Converted Question Detector to a Microservice using Flask

---
+++ question-detector/QuestionDetector_Test copy.py
@@ -0,0 +1,21 @@
+"""
+    python QuestionDetector_Test.py
+"""
+import unittest
+import json
+import QuestionDetector
+
+
+class QuestionDetector_Test(unittest.TestCase):
+    def setUp(self):
+        self.question = QuestionDetector.QuestionDetector()
+
+    def test_if_question(self):
+        params = ['Does some food increase pollen allergy symptoms?']
+
+        response = self.question.predict(params, "features")
+        self.assertIsNotNone(response)
+
+
+if __name__ == '__main__':
+    unittest.main()
